0bc078356840260224de843b098a84f1de69ba3a
phattta <pha.ttta2021@gmail.com>
2024-10-25 13:49:08 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,20 @@
 // next.config.mjs
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'cdn.pixabay.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'magnumx.me',
-        },
-        {
-          protocol: 'https',
-          hostname: 'e7.pngegg.com',
-        },
-      ],
-    },
-  };
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'magnumx.me',
+      },
+      {
+        protocol: 'https',
+        hostname: 'e7.pngegg.com',
+      },
+    ],
+  },
+};
