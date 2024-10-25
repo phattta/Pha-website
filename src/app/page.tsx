e269f186b27582d4aaaa9319140e05071415d6c4
phattta <pha.ttta2021@gmail.com>
2024-10-25 14:09:01 +0700

    Fixed repository structure and handled submodule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Home1 from './home/Home1';
+import Home2 from './home/Home2';
+import Home3 from './home/Home3';
+import Home4 from './home/Home4';
+import Login from './login/Login';
 
-export default function page() {
+export default function HomePage() { 
   return (
-    <div>page</div>
-  )
+    <div>
+      <Home1 />
+      <Home2 />
+      <Home3 />
+      <Home4 />
+      <Login />  
+    </div>
+  );
 }
